fa398755f06bd0d53b2b995cb1bff9767d70d4f1
T5750 <evangel_a@sina.com>
2017-05-23 15:50:20 +0800

    added config-client for SpringCloud.

--- SpringCloud/config-client/src/main/java/com/evangel/Application.java
+++ SpringCloud/config-client/src/main/java/com/evangel/Application.java
@@ -2,14 +2,11 @@ package com.evangel;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
 	}
 }
-// http://localhost:7001/didispace/prod/
-// http://localhost:7001/didispace/dev/
+// http://localhost:7002/from
