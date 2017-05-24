90e4cb23adf666175e33c3c6cba27d1992842f3e
T5750 <evangel_a@sina.com>
2017-05-24 16:35:42 +0800

    updated config-client-eureka and config-server-eureka.

--- SpringCloud/config-client-eureka/src/main/java/com/evangel/Application.java
+++ SpringCloud/config-client-eureka/src/main/java/com/evangel/Application.java
@@ -12,4 +12,10 @@ public class Application {
 	}
 }
 // http://localhost:7002/from
+// Spring Cloud构建微服务架构（四）分布式配置中心（续）
 // POST http://localhost:7002/refresh
+// Spring Cloud构建微服务架构（七）消息总线
+// RabbitMQ Management http://localhost:15672
+// POST http://localhost:7002/bus/refresh
+// POST http://localhost:7002/bus/refresh?destination=customers:9000
+// POST http://localhost:7002/bus/refresh?destination=customers:**
