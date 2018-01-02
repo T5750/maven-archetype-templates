0cb47e902888e57b6cadc4d49cbbf0a976281d1b
T5750 <evangel_a@sina.com>
2018-01-02 13:35:05 +0800

    modified DruidDataSourceConfig with loginPassword.

--- SpringBoot/springboot-others/springboot-swagger-druid/src/main/java/wang/raye/springboot/config/DruidDataSourceConfig.java
+++ SpringBoot/springboot-others/springboot-swagger-druid/src/main/java/wang/raye/springboot/config/DruidDataSourceConfig.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
 
 @Configuration
 @EnableTransactionManagement
@@ -53,5 +55,30 @@ public class DruidDataSourceConfig implements EnvironmentAware {
 			e.printStackTrace();
 		}
 		return datasource;
+	}
+
+	/**
+	 * 注册一个StatViewServlet
+	 * https://github.com/alibaba/druid/issues/1370
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean DruidStatViewServle() {
+		// org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+				new StatViewServlet(), "/druid/*");
+		// 添加初始化参数：initParams
+		// 白名单：
+		servletRegistrationBean.addInitParameter("allow",
+				"127.0.0.1,192.168.100.162,192.168.100.77");
+		// IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not
+		// permitted to view this page.
+		servletRegistrationBean.addInitParameter("deny", "192.168.1.73");
+		// 登录查看信息的账号密码.
+		servletRegistrationBean.addInitParameter("loginUsername", "admin");
+		servletRegistrationBean.addInitParameter("loginPassword", "123456");
+		// 是否能够重置数据.
+		servletRegistrationBean.addInitParameter("resetEnable", "false");
+		return servletRegistrationBean;
 	}
 }
